424b429b0c38074260462a1028e36f6816649ccb
Liaayos <ayo21002@byui.edu>
2024-03-03 02:28:07 -0500

    Completed first part of the project

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
 
+const mongodb = require('./data/contacts');
+
+const dotenv = require("dotenv");
+dotenv.config();
+
 const port = process.env.PORT || 3000;
 
+mongodb.initDB((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => { console.log(`Database is listening and node running on ${port}`) });
+    }
+});
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {console.log(`Running on port ${port}`)});
+// app.listen(3000, () => { console.log(`Running on port ${port}`) });

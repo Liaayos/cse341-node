a076c24235f4983e4aafe60ca73259319d1d1048
Liaayos <ayo21002@byui.edu>
2024-03-09 17:08:56 -0500

    temple-based project

---
+++ models/contacts.js
@@ -0,0 +1,16 @@
+module.exports = (mongoose) => {
+  const Contact = mongoose.model(
+    'contacts',
+    mongoose.Schema(
+      {
+        firstName: String,
+        lastName: String,
+        email: String,
+        favoriteColor: String,
+        birthday: String
+      }
+    )
+  );
+
+  return Contact;
+};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
-const router = require('express').Router();
+const routes = require('express').Router();
+const contact = require('./contact');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
-router.get('/', (req, res) => {res.send('Hello World');});
+routes.use('/api-docs', swaggerUi.serve);
+routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
+routes.use('/contacts', contact);
+routes.use(
+  '/',
+  (docData = (req, res) => {
+    let docData = {
+      documentationURL: 'https://nathanbirch.github.io/nathan-byui-api-docs',
+    };
+    res.send(docData);
+  })
+);
 
-router.use('/contacts', require('./users'));
-
-module.exports = router;
+module.exports = routes;

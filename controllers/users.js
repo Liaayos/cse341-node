3c4c6643c5ef1feb270ded5008a004a1c9c7365e
Liaayos <ayo21002@byui.edu>
2024-03-09 20:19:50 -0500

    added swagger

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,7 @@ const mongodb = require('../data/contacts');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
+  //#swagger.tags=['Users']
     const result = await mongodb.getContacts().db().collection('contacts').find();
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
@@ -12,6 +13,7 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
+   //#swagger.tags=['Users']
     const contactId = new ObjectId(req.params.id);
     const result = await mongodb.getContacts().db().collection('contacts').find({_id: contactId});
     result.toArray().then((contacts) => {
@@ -21,7 +23,57 @@ const getSingle = async (req, res) => {
 
 };
 
+const createUser = async (req, res) => {
+   //#swagger.tags=['Users']
+  const contactId = new ObjectId(req.params.id);
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb.getContacts().db().collection('contacts').insertOne(user);
+  if (response.acknowledged > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while creating the user');
+  }
+};
+
+const updateUser = async (req, res) => {
+   //#swagger.tags=['Users']
+  const contactId = new ObjectId(req.params.id);
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb.getContacts().db().collection('contacts').replaceOne({_id: contactId}, user);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the user');
+  }
+};
+
+const deleteUser = async (req, res) => {
+   //#swagger.tags=['Users']
+  const contactId = new ObjectId(req.params.id);
+  const response = await mongodb.getContacts().db().collection('contacts').deleteOne({_id: contactId});
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the user');
+  }
+};
+
 module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    createUser,
+    updateUser,
+    deleteUser
 };
--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./data/contacts');
 const app = express();
 
-const mongodb = require('./data/contacts');
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With Content-Type, Accept,'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+app.use('/', require('./routes'));
+
 
 const dotenv = require("dotenv");
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+
 
 mongodb.initDB((err) => {
     if (err) {
@@ -19,4 +34,4 @@ mongodb.initDB((err) => {
 
 app.use('/', require('./routes'));
 
-// app.listen(3000, () => { console.log(`Running on port ${port}`) });
+
